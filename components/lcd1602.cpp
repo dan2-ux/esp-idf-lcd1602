84c460900a866eafe4ad271f82617d0b5cba032b
Pham Cong Danh <danhlanhat9@gmail.com>
2026-01-13 23:21:33 +0700

    Update and rename lcd1602.c to lcd1602.cpp

---
+++ components/lcd1602.cpp
@@ -0,0 +1,57 @@
+#include "lcd1602.h"
+
+void lcd::LCD::lcd_init()
+{
+    vTaskDelay(pdMS_TO_TICKS(500));
+    gpio_reset_pin(pin_rs);
+    gpio_reset_pin(pin_en);
+    gpio_reset_pin(pin_d4);
+    gpio_reset_pin(pin_d5);
+    gpio_reset_pin(pin_d6);
+    gpio_reset_pin(pin_d7);
+
+    gpio_set_direction(pin_rs, GPIO_MODE_OUTPUT);
+    gpio_set_direction(pin_en, GPIO_MODE_OUTPUT);
+    gpio_set_direction(pin_d4, GPIO_MODE_OUTPUT);
+    gpio_set_direction(pin_d5, GPIO_MODE_OUTPUT);
+    gpio_set_direction(pin_d6, GPIO_MODE_OUTPUT);
+    gpio_set_direction(pin_d7, GPIO_MODE_OUTPUT);
+
+    sendBit(0, 0x3);
+    vTaskDelay(pdMS_TO_TICKS(5));
+    sendBit(0, 0x3);
+    vTaskDelay(pdMS_TO_TICKS(5));
+    sendBit(0, 0x3);
+    vTaskDelay(pdMS_TO_TICKS(5));
+    sendBit(0, 0x2);
+    vTaskDelay(pdMS_TO_TICKS(5));
+
+    sendByte(0, 0x28);
+    vTaskDelay(pdMS_TO_TICKS(2));
+    sendByte(0, 0x0c);
+    vTaskDelay(pdMS_TO_TICKS(2));
+    sendByte(0, 0x06);
+    vTaskDelay(pdMS_TO_TICKS(2));
+    sendByte(0, 0x01);
+    vTaskDelay(pdMS_TO_TICKS(10));
+}
+
+void lcd::LCD::lcd_clear()
+{
+    sendByte(0, 0x01);
+    esp_rom_delay_us(1);
+}
+
+void lcd::LCD::lcd_print(const char *data)
+{
+    while (*data)
+    {
+        sendByte(1, *data++);
+    }
+}
+
+void lcd::LCD::lcd_set_cursor(uint8_t r, uint8_t c)
+{
+    uint8_t num[] = {0x00, 0x40};
+    sendByte(0, 0x80 + num[r] + c);
+}
