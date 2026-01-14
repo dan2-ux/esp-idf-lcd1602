4fe0459fc676491765416a1100be545e63c70b88
Pham Cong Danh <danhlanhat9@gmail.com>
2026-01-14 09:05:40 +0700

    Update lcd1602.h

--- components/lcd1602.h
+++ components/lcd1602.h
@@ -7,27 +7,31 @@ namespace lcd
     class LCD
     {
     private:
-        gpio_num_t pin_rs;
-        gpio_num_t pin_en;
-        gpio_num_t pin_d4;
-        gpio_num_t pin_d5;
-        gpio_num_t pin_d6;
-        gpio_num_t pin_d7;
+        typedef struct {
+            gpio_num_t pin_rs;
+            gpio_num_t pin_en;
+            gpio_num_t pin_d4;
+            gpio_num_t pin_d5;
+            gpio_num_t pin_d6;
+            gpio_num_t pin_d7;
+        } lcd_pins;
+
+        lcd_pins pins;
         void pulse_en()
         {
-            gpio_set_level(pin_en, 1);
+            gpio_set_level(pins.pin_en, 1);
             esp_rom_delay_us(5);
-            gpio_set_level(pin_en, 0);
+            gpio_set_level(pins.pin_en, 0);
             esp_rom_delay_us(50);
         }
         void sendBit(uint8_t rs_state, uint8_t data)
         {
-            gpio_set_level(pin_rs, rs_state);
+            gpio_set_level(pins.pin_rs, rs_state);
 
-            gpio_set_level(pin_d4, (data >> 0) & 0x01);
-            gpio_set_level(pin_d5, (data >> 1) & 0x01);
-            gpio_set_level(pin_d6, (data >> 2) & 0x01);
-            gpio_set_level(pin_d7, (data >> 3) & 0x01);
+            gpio_set_level(pins.pin_d4, (data >> 0) & 0x01);
+            gpio_set_level(pins.pin_d5, (data >> 1) & 0x01);
+            gpio_set_level(pins.pin_d6, (data >> 2) & 0x01);
+            gpio_set_level(pins.pin_d7, (data >> 3) & 0x01);
 
             pulse_en();
         }
@@ -39,9 +43,7 @@ namespace lcd
         }
 
     public:
-        LCD(gpio_num_t rs, gpio_num_t en,
-            gpio_num_t d4, gpio_num_t d5,
-            gpio_num_t d6, gpio_num_t d7) : pin_rs(rs), pin_en(en), pin_d4(d4), pin_d5(d5), pin_d6(d6), pin_d7(d7) {};
+        LCD(const lcd_pins &p) : pins(p) {};
         void lcd_init();
         void lcd_clear();
 
